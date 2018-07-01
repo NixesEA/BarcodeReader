a6e3bfdc4124dea143dc976964680cd96664bc3c
NixesEA <ermakov.anton201133@gmail.com>
2018-07-01 15:40:51 +0300

    1.07 Add recognition URI

--- app/src/main/java/com/nixesea/barcodereader/MainActivity.java
+++ app/src/main/java/com/nixesea/barcodereader/MainActivity.java
@@ -2,9 +2,17 @@ package com.nixesea.barcodereader;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,8 +20,12 @@ import android.util.SparseArray;
 import android.view.Display;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
@@ -21,13 +33,22 @@ import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import java.io.IOException;
+import java.security.Policy;
 
-public class MainActivity extends AppCompatActivity implements Detector.Processor{
+public class MainActivity extends AppCompatActivity implements Detector.Processor, View.OnClickListener {
 
     private TextView textView;
     private SurfaceView surfaceView;
     private BarcodeDetector barcodeDetector;
     private CameraSource cameraSource;
+    private ImageButton flash;
+    private ImageButton save;
+
+    private Boolean isStartCamera;
+    private String mCameraId;
+    private Boolean isTorchOn = false;
+
+    private CameraManager mCameraManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,29 +59,38 @@ public class MainActivity extends AppCompatActivity implements Detector.Processo
         getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        textView = (TextView) this.findViewById(R.id.textView);
-        surfaceView = (SurfaceView) this.findViewById(R.id.surfaceView);
+        textView = this.findViewById(R.id.textView);
+        surfaceView = this.findViewById(R.id.surfaceView);
+        flash = findViewById(R.id.flashlightButton);
+        save = findViewById(R.id.saveContentButton);
+        save.setOnClickListener(this);
+        flash.setOnClickListener(this);
+        textView.setOnClickListener(this);
 
         barcodeDetector = new BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.ALL_FORMATS).build();
         barcodeDetector.setProcessor(this);
 
-        cameraSource = new CameraSource.Builder(getApplicationContext(), barcodeDetector).
-                setRequestedPreviewSize(1024,720).setAutoFocusEnabled(true)
-                .setRequestedFps(30)
+        cameraSource = new CameraSource.Builder(getApplicationContext(), barcodeDetector)
+                .setFacing(cameraSource.CAMERA_FACING_BACK)
+                .setRequestedPreviewSize(1024, 720)
+                .setAutoFocusEnabled(true)
+                .setRequestedFps(25)
                 .build();
+
 
         final Activity activity = this;
 
         surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
-                try{
-                    if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-                        ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.CAMERA},1024);
+                try {
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.CAMERA}, 1024);
                         return;
                     }
+                    isStartCamera = true;
                     cameraSource.start(surfaceView.getHolder());
-                } catch (IOException ie){
+                } catch (IOException ie) {
                     Log.e("Camera start problem", ie.getMessage());
                 }
             }
@@ -86,9 +116,9 @@ public class MainActivity extends AppCompatActivity implements Detector.Processo
     public void receiveDetections(Detector.Detections detections) {
         final SparseArray<Barcode> barcodes = detections.getDetectedItems();
 
-        if(barcodes.size() != 0){
+        if (barcodes.size() != 0) {
             final StringBuilder sb = new StringBuilder();
-            for(int i =0 ; i<barcodes.size(); ++i){
+            for (int i = 0; i < barcodes.size(); ++i) {
                 sb.append(barcodes.valueAt(i).rawValue).append("\n");
             }
             textView.post(new Runnable() {
@@ -97,6 +127,88 @@ public class MainActivity extends AppCompatActivity implements Detector.Processo
                     textView.setText(sb.toString());
                 }
             });
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.flashlightButton:
+                switchFlash();
+                break;
+            case R.id.saveContentButton:
+
+                break;
+            case R.id.textView:
+                String str = (String) textView.getText();
+                String[] subStr;
+                subStr = str.split(" ");
+                for (String aSubStr : subStr) {
+                    if (aSubStr.indexOf("http") >= 0){
+                        Uri address = Uri.parse(aSubStr);
+                        Intent intent = new Intent(Intent.ACTION_VIEW, address);
+                        startActivity(intent);
+                        break;
+                    }
+                }
+                break;
+        }
+
+    }
+
+    private void switchFlash() {
+        if (isStartCamera) {
+            isStartCamera = false;
+            cameraSource.stop();
+        }
+
+        Boolean isFlashAvailable = getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        if (!isFlashAvailable) {
+            AlertDialog alert = new AlertDialog.Builder(MainActivity.this)
+                    .create();
+            alert.setTitle("Error");
+            alert.setMessage("Your device doesn't support flash light!");
+            alert.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            alert.show();
+        }
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            mCameraId = mCameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mCameraManager.setTorchMode(mCameraId, !isTorchOn);
+                isTorchOn = !isTorchOn;
+                if (isTorchOn) {
+                    flash.setImageResource(R.mipmap.flash_icon);
+                } else {
+                    flash.setImageResource(R.mipmap.flashoff_icon);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        final Activity activity = this;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.CAMERA}, 1024);
+            return;
+        }
+        try {
+            isStartCamera = true;
+            cameraSource.start(surfaceView.getHolder());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
